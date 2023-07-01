185efcfa416aff4e3c727f6568a2ae2f96a3fd1a
Dannie <dadaley19@gmail.com>
2023-07-01 15:49:36 -0500

    rearrange

--- src/data/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file includes work covered by the following copyright and permission notices:
-#
-#  Copyright 2023 Google LLC
-#  Licensed under the Apache License, Version 2.0 (the "License");

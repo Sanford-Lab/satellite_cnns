68aaf008e912ae0d5112f47d6e151e4758b04bab
Dannie <Dadaley19@gmail.com>
2023-07-04 17:29:13 -0500

    add: custom split, custom Subset

--- read_data.py
+++ read_data.py
@@ -1,10 +1,21 @@
 import os
 from glob import glob
-from typing import Any
+from typing import (
+    Any,
+    List,
+    Optional,
+    Sequence,
+    Union,
+    TypeVar
+)
+
+T_co = TypeVar('T_co', covariant=True)
+T = TypeVar('T')
 
 import numpy as np
-from torch.utils.data import Dataset, random_split
+from torch.utils.data import Dataset
 from torch import Generator
+from torch._utils import _accumulate
 
 # Constants.
 SEED = 0
@@ -86,7 +97,7 @@ def test_train_split(data:DatasetFromPath, ratio:float = TEST_TRAIN_RATIO, seed 
     assert(ratio >=0 and ratio <= 1)
     seed_gen = Generator().manual_seed(SEED)
     
-    return random_split(dataset=data, lengths=[ratio, (1-ratio)], generator=seed_gen)
+    return custom_random_split(dataset=data, lengths=[ratio, (1-ratio)], generator=seed_gen)
 
 
 class EmptyDirectoryError(Exception):
@@ -95,3 +106,88 @@ class EmptyDirectoryError(Exception):
                  message=f'Path is empty or does not contain files ending in .npz') -> None:
         self.message = message
         super().__init__(self.message)
+        
+class CustomSubset(Dataset[T_co]):
+    r"""
+    Subset of a dataset at specified indices.
+
+    Args:
+        dataset (Dataset): The whole Dataset
+        indices (sequence): Indices in the whole set selected for subset
+    """
+    dataset: Dataset[T_co]
+    indices: Sequence[int]
+
+    def __init__(self, dataset: Dataset[T_co], indices: Sequence[int]) -> None:
+        self.dataset = dataset
+        self.indices = indices
+
+    def __getitem__(self, idx):
+        if isinstance(idx, list):
+            return self.dataset[[self.indices[i] for i in idx]]
+        elif isinstance(idx, str) & (idx == 'inputs' or idx == 'labels'):
+            if idx == 'inputs': 
+                return self.dataset['inputs'][[self.indices[i] for i in idx]]
+            return self.dataset['labels'][[self.indices[i] for i in idx]]
+        return self.dataset[self.indices[idx]]
+
+    def __len__(self):
+        return len(self.indices)
+
+
+
+def custom_random_split(dataset: Dataset[T], lengths: Sequence[Union[int, float]],
+                 generator: Optional[Generator] = Generator().manual_seed(0)) -> List[CustomSubset[T]]:
+    """
+    Same as torch.utils.data.dataset.random_split except returns custom subclass and 
+    and uses Generator().manual_seed(0) as default generator
+    Randomly split a dataset into non-overlapping new datasets of given lengths.
+
+    If a list of fractions that sum up to 1 is given,
+    the lengths will be computed automatically as
+    floor(frac * len(dataset)) for each fraction provided.
+
+    After computing the lengths, if there are any remainders, 1 count will be
+    distributed in round-robin fashion to the lengths
+    until there are no remainders left.
+
+    Optionally fix the generator for reproducible results, e.g.:
+
+    Example:
+        >>> # xdoctest: +SKIP
+        >>> generator1 = torch.Generator().manual_seed(42)
+        >>> generator2 = torch.Generator().manual_seed(42)
+        >>> random_split(range(10), [3, 7], generator=generator1)
+        >>> random_split(range(30), [0.3, 0.3, 0.4], generator=generator2)
+
+    Args:
+        dataset (Dataset): Dataset to be split
+        lengths (sequence): lengths or fractions of splits to be produced
+        generator (Generator): Generator used for the random permutation.
+    """
+    if np.isclose(sum(lengths), 1) and sum(lengths) <= 1:
+        subset_lengths: List[int] = []
+        for i, frac in enumerate(lengths):
+            if frac < 0 or frac > 1:
+                raise ValueError(f"Fraction at index {i} is not between 0 and 1")
+            n_items_in_split = int(
+                np.floor(len(dataset) * frac)  # type: ignore[arg-type]
+            )
+            subset_lengths.append(n_items_in_split)
+        remainder = len(dataset) - sum(subset_lengths)  # type: ignore[arg-type]
+        # add 1 to all the lengths in round-robin fashion until the remainder is 0
+        for i in range(remainder):
+            idx_to_add_at = i % len(subset_lengths)
+            subset_lengths[idx_to_add_at] += 1
+        lengths = subset_lengths
+        for i, length in enumerate(lengths):
+            if length == 0:
+                print(f"Length of split at index {i} is 0. "
+                              f"This might result in an empty dataset.")
+
+    # Cannot verify that dataset is Sized
+    if sum(lengths) != len(dataset):    # type: ignore[arg-type]
+        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
+
+    indices = randperm(sum(lengths), generator=generator).tolist()  # type: ignore[call-overload]
+    return [CustomSubset(dataset, indices[offset - length : offset]) for offset, length in zip(_accumulate(lengths), lengths)]

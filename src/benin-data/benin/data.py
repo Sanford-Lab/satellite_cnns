f609b9dd7232f0242f6f4d8d0ed3bf4d97e1b56f
Sanford-Lab <105933693+Sanford-Lab@users.noreply.github.com>
2024-06-17 13:18:14 -0400

    test simpleComposite approach

--- src/benin-data/benin/data.py
+++ src/benin-data/benin/data.py
@@ -105,15 +105,22 @@ def get_inputs_image() -> ee.Image:
     # Grab the Benin feature (shape of country)
     benin_shape = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017").filter(ee.Filter.eq('country_na','Benin')).set('ORIG_FID',0)
 
-    # Prepare the cloud masked LANDSAT 7 median composite image
-    benin_input = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR").filterDate('2006-01-01', '2008-12-31')
+    # # Prepare the cloud masked LANDSAT 7 median composite image
+    # benin_input = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR").filterDate('2006-01-01', '2008-12-31')
 
-    # Remove clouds, clip it to the outline of Benin (with buffer)
-    benin_input = (benin_input.map(mask_clouds_landsat).median().clip(benin_shape.geometry().buffer(10000)))
+    # # Remove clouds, clip it to the outline of Benin (with buffer)
+    # benin_input = (benin_input.map(mask_clouds_landsat).median().clip(benin_shape.geometry().buffer(10000)))
 
     # # Use existing L7 annual composite
     # benin_input = ee.ImageCollection('LANDSAT/LE7_TOA_3YEAR').filterDate('2008-01-01', '2008-12-31').first()
     # benin_input = (benin_input.clip(benin_shape.geometry().buffer(10000)))
+
+    l7_filtered = ee.ImageCollection('LANDSAT/LE07/C02/T1').filterDate('2006-1-1', '2008-12-31');
+
+    benin_input = (ee.Algorithms.Landsat.simpleComposite({
+    collection: l7_filtered,
+    asFloat: true
+    }).clip(benin_shape.geometry().buffer(10000))).;
 
     # Create NDVI band, rename RGB
     ndvi_img = benin_input.normalizedDifference(thermalBands).rename(['NDVI'])
